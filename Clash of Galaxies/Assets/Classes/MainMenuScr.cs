ce592b1a5ef059eb85a7c50ef27f022240479088
Alexey Artemyev <alexey.artem.alexey.artem@gmail.com>
2022-04-09 18:14:12 +0300

    Add full menu layout

--- Clash of Galaxies/Assets/Classes/MainMenuScr.cs
+++ Clash of Galaxies/Assets/Classes/MainMenuScr.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScr : MonoBehaviour
 {
+    public GameObject BackMenuPanel, GameRulesPanel, SettingsPanel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,11 +31,20 @@ public class MainMenuScr : MonoBehaviour
 
     public void SettingsClick() 
     {
-
+        BackMenuPanel.SetActive(true);
+        SettingsPanel.SetActive(true);
     }
 
     public void RulesClick() 
     {
+        BackMenuPanel.SetActive(true);
+        GameRulesPanel.SetActive(true);
+    }
 
+    public void BackMenuClick() 
+    {
+        GameRulesPanel.SetActive(false);
+        SettingsPanel.SetActive(false);
+        BackMenuPanel.SetActive(false);
     }
 }
